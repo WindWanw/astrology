510fe1dbf40eeb1ae7be8b4255f44e96bd873688
windWanw <15720951365@163.com>
2020-05-22 10:58:53 +0800

    project

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\admin\controller;
+
+use app\admin\controller\Base;
+
+class Login extends Base
+{
+
+    public function login()
+    {
+        
+        echo 123;
+
+    }
+}
