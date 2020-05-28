43e8727bd6e25077eabc32e0ff22336d0bca4d51
WindWanw <2438001922@qq.com>
2020-05-28 21:37:05 +0800

    ww

--- application/admin/controller/About.php
+++ application/admin/controller/About.php
@@ -8,6 +8,9 @@ use app\admin\model\About as AboutModel;
 class About extends Base
 {
 
+    /**
+     * 关于我们列表
+     */
     public function getAboutList()
     {
         $query = AboutModel::with('user');
@@ -18,10 +21,13 @@ class About extends Base
         return success(['list' => $list, 'total' => $count]);
     }
 
+    /**
+     * 添加关于我们内容
+     */
     public function addAboutInfo()
     {
         $data = input();
-        $data['user_id']=$this->getUserId();
+        $data['user_id'] = $this->getUserId();
 
         if (AboutModel::create($data)) {
             return success('添加成功');
@@ -30,8 +36,22 @@ class About extends Base
         return error('添加失败');
     }
 
+    /**
+     * 修改
+     */
     public function editAboutInfo()
     {
+        $data = [
+            'title' => input('title'),
+            'content' => input('content'),
+            'image' => input('image'),
+            'status' => input('status'),
+        ];
 
+        if (AboutModel::where('id', input('id'))->update($data)) {
+            return success('更新成功');
+        }
+
+        return error('更新失败');
     }
 }
